56af836d565c7b2d120f23e8b183fb25dc37b673
Mukul Sharma <72088102+mukul9627@users.noreply.github.com>
2024-02-26 18:46:39 +0000

    Memory Stack heap

--- comparisonDatatypesSummary.js
+++ comparisonDatatypesSummary.js
@@ -10,8 +10,8 @@ const  outsideTemp = null;
 
 let valuedefine;
 
-let name = symbol(123)
-let sirname = symbol(123)
+let name = Symbol(123)
+let sirname = Symbol(123)
 
 const largNumber = 111111112222233333444444n;
 
@@ -29,3 +29,33 @@ const obj = {
 const myFunction = function () {
     console.log('hi all Stu')
 }
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++==
+
+// Stack (Primitive) 
+
+let myName = "Mukul Sharma"
+
+let mySecondName = myName;
+
+mySecondName = 'bhola'
+
+console.log(myName)
+console.log(mySecondName)
+
+
+
+// Heap (Non Primitive)
+
+let firstName = {
+    email: "mukul@gmail.com",
+    id: 12345
+}
+
+let secondName = firstName;
+
+secondName.id = 2345;
+
+console.log(firstName)
+console.log(secondName)
