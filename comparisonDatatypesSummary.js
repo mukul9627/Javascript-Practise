5b093ccfe9c5180f0979bf152d4d9096e295b4b7
Mukul Sharma <72088102+mukul9627@users.noreply.github.com>
2024-02-26 18:23:28 +0000

    Primitive Non primitive

---
+++ comparisonDatatypesSummary.js
@@ -0,0 +1,31 @@
+// Primitive  (Call by value)
+
+// 7 types String, number, boolean, null, undefined, Symbol, bigInt
+
+const Name = "mukul"
+
+const score = 100;
+const isLoginedIn = true;
+const  outsideTemp = null;
+
+let valuedefine;
+
+let name = symbol(123)
+let sirname = symbol(123)
+
+const largNumber = 111111112222233333444444n;
+
+// Non Primitive (Call by Reference)
+
+// Array, Objects, Function
+
+const arr = [1,2,3,4,5,6,7,8]
+
+const obj = {
+    name: 'Mukul',
+    sarName: 'sharma'
+}
+
+const myFunction = function () {
+    console.log('hi all')
+}
