892541de944302c20b8bfafea74228fd53f70c05
Mukul Sharma <72088102+mukul9627@users.noreply.github.com>
2024-03-09 19:32:22 +0000

    Math or Number

---
+++ Numbermath.js
@@ -0,0 +1,37 @@
+// --------------------Number------------------------------------------//
+
+// const matchscore = 400
+// console.log(matchscore);
+
+// const balance = new Number(100)
+// console.log(typeof balance.toString())
+// console.log(balance.toFixed(2))
+
+// const  otherNumber = 12.9999
+
+// console.log(otherNumber.toPrecision(2))
+
+// const hundreds = 20000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ----------------------------Math----------------------//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.4))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+
+console.log(Math.random());
+
+const min = 10;
+const max = 20;
+const mathr = Math.random();
+
+console.log(mathr)
+
+console.log(Math.floor(mathr * (max - min + 1)) + min )
+
+
--- String.js
+++ String.js
@@ -1,1 +1,14 @@
 //Stringf
+
+// let mukul = 'mukul sharma';
+
+// console.log(typeof mukul)
+
+// const test1 = new String ("MukulSharma")
+
+// console.log(typeof test1)
+
+
+const test2 = new String ("Mukul-katiyan-sharma")
+
+console.log(test2.split('mukul'))
