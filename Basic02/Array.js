42d36462fe397e6e81f5fe7e53e3004164d930dd
Mukul Sharma <72088102+mukul9627@users.noreply.github.com>
2024-03-11 18:18:24 +0000

    array methods

---
+++ Basic02/Array.js
@@ -0,0 +1,35 @@
+// Array 
+
+// const arr = [0, 1, 2, 3, 4]
+
+// console.log(typeof arr[2])
+
+// console.log(arr.length)
+
+// Array Methods
+
+ const myArr = [1, 2, 3, 4, 5, 6] 
+
+//  myArr.push(7) //(Prototype) push  add the value in a array
+ // myArr.pop() //(Prototyype) pop remove the array lest element
+ 
+//  myArr.unshift(8)
+//  myArr.shift() 
+ 
+//  console.log(myArr)
+
+//  const myNewArr = myArr.join()
+
+//  console.log(typeof myNewArr)
+
+// -------------------slice, splice---------------------//
+
+const mynew1 = myArr.slice(1, 4)
+
+// console.log("o", myArr)
+// console.log("A", mynew1);
+
+
+// const mynew2 = myArr.splice(1, 4) //The splice() method of Array instances changes the contents of an array by removing or replacing existing elements and/or adding new elements 
+// console.log("o", myArr)
+// console.log("A", mynew2);
